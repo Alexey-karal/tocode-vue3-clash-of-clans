dbc39bba06b4038c3a5234f69f4ce9c7aec70cad
Алексей Карагодин <karal@MacBook-Pro-Aleksej.local>
2022-05-07 02:12:38 +0500

    initial commit

---
+++ src/App.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="wrapper">
+    <TheHeader />
+    <div class="wrapper-content">
+      <div class="container">
+        <router-view />
+      </div>
+    </div>
+    <TheFooter />
+  </div>
+</template>
+
+<script>
+import TheHeader from '@/components/TheHeader'
+import TheFooter from '@/components/TheFooter'
+export default {
+  components: {
+    TheHeader: TheHeader,
+    TheFooter: TheFooter
+  }
+}
+</script>
+
+<style lang="scss"></style>
